a1853ddee450053ccc2110abcb1d0b0bff4ba7f4
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-12-11 21:51:54 +0700

    Add link to main app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div>
       <h1>Taskman</h1>
       <p>Task manager app</p>
+      <Link href="/app">Main app</Link>
     </div>
   );
 }
