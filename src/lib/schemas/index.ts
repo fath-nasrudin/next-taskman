b690ab08071a0385d17bcdfdeb4952c5f735930a
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-12-12 09:27:08 +0700

    Add taskFrom schema

--- src/lib/schemas/index.ts
+++ src/lib/schemas/index.ts
@@ -6,3 +6,11 @@ export const projectFormSchema = z.object({
 });
 
 export type ProjectFormValues = z.infer<typeof projectFormSchema>;
+
+export const taskFormSchema = z.object({
+  name: z.string().min(1),
+  projectId: z.string().cuid(),
+  isDone: z.boolean().optional(),
+});
+
+export type TaskFormValues = z.infer<typeof taskFormSchema>;
