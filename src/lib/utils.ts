177b880d9d77e4063218937c33b4f910713170f1
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-12-13 16:19:53 +0700

    Add createSlug util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const createSlug = ({ name, id }: { name: string; id: string }) => {
+  return `${name.toLowerCase().split(' ').join('-')}-${id}`;
+};
