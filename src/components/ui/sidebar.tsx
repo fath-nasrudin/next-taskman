b2545a56533978cfb3b8a184d73e74dd749ae0bf
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-12-14 21:02:13 +0700

    Add sidebar title to remove radix dialogtitle warning

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -206,6 +206,7 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
+            <SheetTitle className="invisible">Sidebar</SheetTitle>
             <div className="flex h-full w-full flex-col">{children}</div>
           </SheetContent>
         </Sheet>
