813b1ec751683b31b257dee2dbf586a3b783a993
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-12-14 20:52:09 +0700

    Move prefetch to layout

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -13,6 +13,11 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from '@/components/ui/sidebar';
+import { getProjectsByUserId } from '@/lib/api';
+import { auth } from '@/lib/auth';
+import { getQueryClient } from '@/lib/query-client';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
 export default async function MainAppLayout({
@@ -20,29 +25,45 @@ export default async function MainAppLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+  const queryClient = getQueryClient();
+
+  if (!session) redirect('/login');
+
+  queryClient.prefetchQuery({
+    queryKey: ['projects'],
+    queryFn: async () => {
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      return getProjectsByUserId(session.user.id);
+    },
+    staleTime: 5 * 60 * 1000,
+  });
+
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
-      </SidebarInset>
-    </SidebarProvider>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </header>
+          <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
+        </SidebarInset>
+      </SidebarProvider>
+    </HydrationBoundary>
   );
 }
--- src/components/nav-projects.tsx
+++ src/components/nav-projects.tsx
@@ -1,26 +1,6 @@
-import { auth } from '@/lib/auth';
 import { NavProjectsClient } from './nav-projects.client';
-import { redirect } from 'next/navigation';
-import { getQueryClient } from '@/lib/query-client';
-import { getProjectsByUserId } from '@/lib/api';
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import React from 'react';
 
 export async function NavProjects() {
-  const session = await auth();
-  if (!session) redirect('/login');
-
-  const queryClient = getQueryClient();
-  queryClient.prefetchQuery({
-    queryKey: ['projects'],
-    queryFn: async () => {
-      await new Promise((resolve) => setTimeout(resolve, 5000));
-      return getProjectsByUserId(session.user.id);
-    },
-  });
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <NavProjectsClient />
-    </HydrationBoundary>
-  );
+  return <NavProjectsClient />;
 }
