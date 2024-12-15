941ef8ff79245d23756fb09c9aadf25ac8889393
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-12-15 21:10:46 +0700

    Add padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
+    <div className="min-h-screen p-4 text-center flex flex-col justify-center items-center">
       <h1 className="text-4xl font-bold text-red-500">Taskman</h1>
       <p>A task manager app to help you manage your day</p>
       <Link href="/app" className="text-red-500">
