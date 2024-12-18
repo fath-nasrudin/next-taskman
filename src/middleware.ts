38c518a3ee68062758c75e7ff84dc46888a87692
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-12-18 13:00:45 +0700

    Add auth redirect
    Redirect to /app when authenticated user try to
    access /login or /register
    
    redirect to /login when not authenticated user
    try to access protected pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,23 @@
 import NextAuth from 'next-auth';
 import authConfig from '@/lib/auth/auth.config';
+import { NextResponse } from 'next/server';
 
 const { auth } = NextAuth(authConfig);
-export const middleware = auth;
+
+export default auth((req) => {
+  const isAuth = req.auth;
+  const isAuthPage =
+    req.nextUrl.pathname === '/login' || req.nextUrl.pathname === '/register';
+  if (!isAuth && !isAuthPage) {
+    const newUrl = new URL('/login', req.nextUrl.origin);
+    return NextResponse.redirect(newUrl);
+  }
+
+  if (isAuth && isAuthPage) {
+    return NextResponse.redirect(new URL('/app', req.nextUrl.origin));
+  }
+});
 
 export const config = {
-  matcher: ['/protected'],
+  matcher: ['/app', '/protected', '/login', '/register'],
 };
