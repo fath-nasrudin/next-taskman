6c8e9fd6fd05e67b8444e58eb08557d94b467f05
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-12-20 17:06:46 +0700

    Add toast when project creation

--- src/app/app/project-create.tsx
+++ src/app/app/project-create.tsx
@@ -14,6 +14,7 @@ import { ProjectFormValues } from '@/lib/schemas';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Edit2Icon } from 'lucide-react';
 import React, { useState } from 'react';
+import { toast } from 'sonner';
 
 export const CreateProject = ({ children }: { children?: React.ReactNode }) => {
   const [open, setOpen] = useState(false);
@@ -26,7 +27,16 @@ export const CreateProject = ({ children }: { children?: React.ReactNode }) => {
     }: {
       projectFormValues: ProjectFormValues;
     }) => {
-      await createProjectAction(projectFormValues);
+      const { error, data } = await createProjectAction(projectFormValues);
+
+      if (error) {
+        toast.error(error.message, { position: 'top-center' });
+      }
+      if (data) {
+        toast.success('Project Successfully created', {
+          position: 'top-center',
+        });
+      }
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['projects'] });
